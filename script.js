738fff36b02d8ded0cf0f95fbb8d37e35dd211bf
unknown <santiagocornue.p.e.t20@gmail.com>
2024-02-26 18:41:56 -0300

    cambios

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let Productos = [];
+let Productos = [ { id: 1, nombre: "limones", precio: 1200,cantidad:"30" },
+{ id: 2, nombre: "manzanas", precio: 1800,cantidad:"30"},
+{ id: 3, nombre: "peras", precio: 2800,cantidad:"30"},
+{ id: 4, nombre: "pepino", precio: 1500,cantidad:"30"},
+{ id: 5, nombre: "berenjenas", precio: 1200,cantidad:"30"},
+{ id: 6, nombre: "no se", precio: 1900,cantidad:"30" },
+{ id: 7, nombre: "sandias", precio: 5800,cantidad:"30"}];
 
 function agregarProducto() {
     let form = document.getElementById('myForm');
     let id = form.id.value;
     let cantidad = Number(form.quantity.value);
-    let valor = Number(form.value.value);
+    let precio = Number(form.value.value);
     let nombre = form.name.value;
 
     let productoExistente = Productos.find(producto => producto.id === id);
@@ -16,7 +22,7 @@ function agregarProducto() {
     let objeto = {
         id: id,
         cantidad: cantidad,
-        valor: valor,
+        precio: precio,
         nombre: nombre,
     };
 
@@ -30,9 +36,9 @@ document.querySelector('.formulario').addEventListener('submit', function(event)
     let nombre = document.getElementById('name').value;
     let id = document.getElementById('id').value;
     let cantidad = Number(document.getElementById('quantity').value);
-    let valor = Number(document.getElementById('value').value);
+    let precio = Number(document.getElementById('value').value);
 
-    if (!nombre || !id || !cantidad || cantidad < 1 || !valor || valor < 0) {
+    if (!nombre || !id || !cantidad || cantidad < 1 || !precio || precio < 0) {
         alert('Falta rellenar un espacio.');
     } else {
         agregarProducto();
@@ -43,7 +49,7 @@ function mostrarProductos() {
     if (Productos.length === 0) {
         alert('No hay productos.');
     } else {
-        let productosString = Productos.map(producto => `ID: ${producto.id}, Nombre: ${producto.nombre}, Cantidad: ${producto.cantidad}, Valor: ${producto.valor}`).join('\n');
+        let productosString = Productos.map(producto => `ID: ${producto.id}, Nombre: ${producto.nombre}, Cantidad: ${producto.cantidad}, Valor: ${producto.precio}`).join('\n');
         alert(productosString);
     }
 }
@@ -58,4 +64,18 @@ function eliminarProducto() {
     }
 }
 
+function buscarProducto(arr, filtro) {
+    if (filtro.trim() === "") {
+        alert("Por favor, ingresa un valor válido.");
+        return;
+    }
+    const encontrado = arr.find((el) => {
+        return el.nombre.includes(filtro);
+    });
+    if (encontrado === undefined) {
+        alert("No se encontró ningún producto con ese nombre.");
+    } else {
+        alert(JSON.stringify(encontrado, null, 2));
+    }
+}
 
